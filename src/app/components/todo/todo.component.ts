7dd493989b2a48ae8ff8c6ca1a0b9c4ea4ceb027
Patrick Johnson <pjohnson24@icloud.com>
2020-07-08 12:21:57 -0400

    prettier applied

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,20 +1,19 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
-   
   todos: Todo[] = [];
 
   constructor() {
-    let todo1: Todo = { task: "Walk the dog", completed: false};
-    let todo2: Todo = { task: "Clean my car", completed: false};
-    let todo3: Todo = { task: "Go to class", completed: false};
-    let todo4: Todo = { task: "Make dinner", completed: false};
+    let todo1: Todo = { task: 'Walk the dog', completed: false };
+    let todo2: Todo = { task: 'Clean my car', completed: false };
+    let todo3: Todo = { task: 'Go to class', completed: false };
+    let todo4: Todo = { task: 'Make dinner', completed: false };
 
     this.todos.push(todo1);
     this.todos.push(todo2);
@@ -22,20 +21,17 @@ export class TodoComponent {
     this.todos.push(todo4);
   }
 
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addTodo(newTodo) {
-    if(newTodo.value !=""){
-      let todo: Todo = {task: newTodo.value, completed: false}
+    if (newTodo.value != '') {
+      let todo: Todo = { task: newTodo.value, completed: false };
       this.todos.push(todo);
-      newTodo.value ="";
+      newTodo.value = '';
     }
-  
   }
 
-  completeTodo(todo){
+  completeTodo(todo) {
     todo.completed = !todo.completed;
   }
 }
